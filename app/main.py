5a4e2e62d0ef685431667c5835250a7de318815a
andreaslagocki <andreas@lagocki.de>
2023-11-24 15:21:03 +0100

    changed to fastapi_users

--- app/main.py
+++ app/main.py
@@ -1,76 +1,28 @@
-from dotenv import find_dotenv, load_dotenv
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.responses import HTMLResponse
-from sqlmodel import Session
-from . import database, models_and_schemas, crud, auth, sendmail
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends, FastAPI
 
-load_dotenv(find_dotenv())
+from app.db import User, create_db_and_tables
+from app.schemas import UserCreate, UserRead, UserUpdate
+from app.users import active_user, auth_backend, fastapi_users
 
 app = FastAPI(
     title="Andreas Lagocki | DigitalArt",
 )
+app.include_router(fastapi_users.get_auth_router(auth_backend), tags=["Authentication"])
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate), tags=["Authentication"]
+)
+app.include_router(fastapi_users.get_reset_password_router(), tags=["Authentication"])
+app.include_router(fastapi_users.get_verify_router(UserRead), tags=["Authentication"])
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate), tags=["User"], prefix="/users"
+)
+
+
+@app.get("/customer")
+async def customer_route(user: User = Depends(active_user)):
+    return {"message", f"Hallo von {user.username} mit {user.email}"}
 
 
 @app.on_event("startup")
-def startup_event():
-    database.create_db_and_tables()
-
-
-@app.post("/register", tags=["Default"])
-def register_user(user: models_and_schemas.UserSchema, db: Session = Depends(database.get_db)):
-    db_user = crud.create_user(db=db, user=user)
-    token = auth.create_access_token(user=db_user)
-    #sendmail.send_mail(to=user.email, token=token, username=user.username)
-    return db_user
-
-
-@app.post("/login", tags=["Default"])
-def login(db: Session = Depends(database.get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    db_user = crud.get_user_by_username(db=db, username=form_data.username)
-    if not db_user:
-        raise HTTPException(status_code=401, detail="Anmeldeinformationen nicht korrekt")
-    if auth.verify_password(form_data.password, db_user.hashed_password):
-        token = auth.create_access_token(db_user)
-        return {"access_token": token, "token_typ": "Bearer"}
-    raise HTTPException(status_code=401, detail="Anmeldeinformationen nicht korrekt")
-
-
-@app.get("/verify/{token}", response_class=HTMLResponse, tags=["Default"])
-def verify_user(token: str, db: Session = Depends(database.get_db)):
-    claims = auth.decode_token(token)
-    username = claims.get('sub')
-    db_user = crud.get_user_by_username(db=db, username=username)
-    db_user.is_active = True
-    db.commit()
-    db.refresh(db_user)
-    return f"""
-    <html>
-        <head>
-            <title>Bestätigung der Registrierung</title>
-        </head>
-        <body>
-            <h2>Aktivierung von {username} erfolgreich!</h2>
-            <a href="https://www.lagocki.de">
-                Zurück
-            </a>
-        </body>
-    </html>
-    """
-
-# @app.get("/users")
-def get_all_user(db: Session = Depends(database.get_db), tags=["Default"]):
-    users = crud.get_users(db=db)
-    return users
-
-
-@app.get("/customer/{id}", dependencies=[Depends(auth.check_active)], tags=["Customer"], description="Get Customer Data by ID")
-def get_customer(id:int, db: Session = Depends(database.get_db)):
-    users = crud.get_user_by_id(db=db, userid=id)
-    return users
-
-
-@app.get("/admin", dependencies=[Depends(auth.check_admin)], tags=["Admin"])
-def secured(db: Session = Depends(database.get_db)):
-    users = crud.get_users(db=db)
-    return users
+async def on_startup():
+    await create_db_and_tables()
---
+++ app/users.py
@@ -0,0 +1,39 @@
+from fastapi import Depends
+from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    CookieTransport,
+    JWTStrategy,
+)
+from fastapi_users.db import SQLAlchemyUserDatabase
+
+from app.db import get_user_db
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+
+class UserManager(UUIDIDMixin, BaseUserManager):
+    reset_password_token_secret = SECRET_KEY
+    verification_token_secret = SECRET_KEY
+
+
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
+
+
+cookie_transport = CookieTransport(cookie_httponly=True, cookie_secure=True)
+
+
+def get_jwt():
+    return JWTStrategy(secret=SECRET_KEY, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt", transport=cookie_transport, get_strategy=get_jwt
+)
+
+
+fastapi_users = FastAPIUsers(get_user_manager, [auth_backend])
+
+
+active_user = fastapi_users.current_user(active=True)
