b5df52375ef2db09e4817884413c243958c5490b
andreaslagocki <andreas@lagocki.de>
2024-01-18 19:26:27 +0100

    store address data from customer

--- app/Model/customeraddressmodel.py
+++ app/Model/customeraddressmodel.py
@@ -19,14 +19,14 @@ class CustomerAddressModel(Base):
         nullable=False,
         unique=True,
     )
-    forename = Column(String(125), index=True, nullable=False)
+    forename = Column(String(125), nullable=False)
     lastname = Column(String(125), index=True, nullable=False)
-    street = Column(String(125), index=True, nullable=False)
-    number = Column(Integer(), index=True, nullable=False)
+    street = Column(String(125), nullable=False)
+    number = Column(String(5), nullable=False)
     city = Column(String(125), index=True, nullable=False)
-    zip = Column(Integer(), index=True, nullable=False)
-    country = Column(String(125), index=True, nullable=True)
-    phone = Column(String(20), index=True, nullable=True)
+    zip = Column(Integer(), nullable=False)
+    country = Column(String(125), nullable=True)
+    phone = Column(String(20), nullable=True)
     created = Column(DateTime, default=datetime.utcnow)
     customer_id = Column(ForeignKey("user.id"), index=True, nullable=False)
 
